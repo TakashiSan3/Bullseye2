84aa73eb93fc772a4df725e2ad90ed624bc006b7
TakashiSan <takashi_fujita_web@icloud.com>
2020-04-14 17:19:38 +0900

    added about view

--- Bullseye2/AboutViewController.swift
+++ Bullseye2/AboutViewController.swift
@@ -5,13 +5,23 @@
 
 
 import UIKit
+import WebKit
 
 class AboutViewController: UIViewController {
+    
+    @IBOutlet weak var webView: WKWebView!
 
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        if let url = Bundle.main.url(forResource: "Bullseye", withExtension: "html") {
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
     }
 
 }
